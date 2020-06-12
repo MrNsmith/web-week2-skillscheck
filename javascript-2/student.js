811382aa531afbc1f63053d4cc3118cfc32f9e5f
MrNsmith <nicholas88smith@gmail.com>
2020-06-12 13:51:17 -0600

    java three done

--- javascript-2/student.js
+++ javascript-2/student.js
@@ -21,15 +21,15 @@ const name = "Nicholas";
 //Create a variable called 'phrase' that is initialized as an empty string. 
 
 // Code Here
-const phrase = "";
+let phrase = "";
 //////////////////STEP 5////////////////////
 //Create an if statement that checks to see if the lovesCode variable is equal to true. If it is, set the phrase variable created above to 'I love code', it it's not true set the phrase variable to 'I do not love to code'
 
 // Code Here
 if (lovesCode === true){
-    return phrase = "I love to code";
+     phrase = "I love to code";
 }else {
-    return phrase = "I do not love to code";
+     phrase = "I do not love to code";
 }
 //////////////////Wrap Up////////////////////
 // Refresh the index.html page in your browser. You should now see a poem that uses the variables from above.
